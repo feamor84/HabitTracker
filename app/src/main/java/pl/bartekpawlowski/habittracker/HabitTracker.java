99b814f6fe61e7cd1d3bdaed7bfa163e0ef206a7
feamor84 <mail@bartekpawlowski.pl>
2017-07-18 11:32:32 +0200

    Move helper methods to HabitTrackerDbHelper

--- app/src/main/java/pl/bartekpawlowski/habittracker/HabitTracker.java
+++ app/src/main/java/pl/bartekpawlowski/habittracker/HabitTracker.java
@@ -3,33 +3,52 @@ package pl.bartekpawlowski.habittracker;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import pl.bartekpawlowski.habittracker.data.HabitTrackerContract.HabitTrackerEntry;
 import pl.bartekpawlowski.habittracker.data.HabitTrackerDbHelper;
 
 public class HabitTracker extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    // Tag for logging
+    private static final String LOG_TAG = HabitTracker.class.getSimpleName();
 
     // Instance of HabitTrackerDbHelper
     private HabitTrackerDbHelper mDbHelper;
 
-    // Method to insert data into SQLight table
-    public void insertHabit() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    }
+        mDbHelper = new HabitTrackerDbHelper(this);
 
-    // Method to get Cursor
-    public Cursor getCursor() {
-        Cursor cursor =  null;
+        mDbHelper.insertHabit("Running", 1500369231, 1500412431, 0);
+        mDbHelper.insertHabit("Walk with dog", 1500412431, 1500413631, 0);
 
-        return cursor;
+        displayHabits();
     }
 
     // Method to display data for database
     public void displayHabits() {
+        Cursor results = mDbHelper.getAllHabits();
 
+        try {
+            int nameColumnIndex = results.getColumnIndex(HabitTrackerEntry.HABIT_NAME);
+            int startTimeColumnIndex = results.getColumnIndex(HabitTrackerEntry.HABIT_START_TIME);
+            int durationColumnIndex = results.getColumnIndex(HabitTrackerEntry.HABIT_DURATION);
+            int isFinishedColumnIndex = results.getColumnIndex(HabitTrackerEntry.IS_HABIT_FINISHED);
+
+            while (results.moveToNext()) {
+                Log.i(LOG_TAG,
+                        "Habit: "
+                                + results.getString(nameColumnIndex) + ", "
+                                + results.getInt(startTimeColumnIndex) + ", "
+                                + results.getInt(durationColumnIndex) + ", "
+                                + results.getInt(isFinishedColumnIndex) + "\n"
+                );
+            }
+        } finally {
+            results.close();
+        }
     }
 }
--- app/src/main/java/pl/bartekpawlowski/habittracker/data/HabitTrackerDbHelper.java
+++ app/src/main/java/pl/bartekpawlowski/habittracker/data/HabitTrackerDbHelper.java
@@ -1,6 +1,8 @@
 package pl.bartekpawlowski.habittracker.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -35,5 +37,32 @@ public class HabitTrackerDbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL(DROP_DB);
+    }
+
+    /**
+     * @param name
+     * @param start
+     * @param duration
+     * @param isFinished
+     */
+    public void insertHabit(String name, int start, int duration, int isFinished) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(HabitTrackerEntry.HABIT_NAME, name);
+        contentValues.put(HabitTrackerEntry.HABIT_START_TIME, start);
+        contentValues.put(HabitTrackerEntry.HABIT_DURATION, duration);
+        contentValues.put(HabitTrackerEntry.IS_HABIT_FINISHED, isFinished);
+
+        sqLiteDatabase.insert(HabitTrackerEntry.TABLE_NAME, null, contentValues);
+    }
+
+    /**
+     * Method @return all records from habit table
+     */
+    public Cursor getAllHabits() {
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+
+        return sqLiteDatabase.query(HabitTrackerEntry.TABLE_NAME, null, null, null, null, null, null);
     }
 }
